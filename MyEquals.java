5fdd1e883b5fa323c374250cb29d64cb9df31715
ashutosh kumar <ashutoshimsec@gmail.com>
2015-08-20 14:27:21 +0530

    Create MyEquals.java

---
+++ MyEquals.java
@@ -0,0 +1,23 @@
+package dataStructure;
+
+
+public class MyEquals {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str1 = new String("abc");
+		String str2 = new String("abc");
+		
+		if(str1.equals(str2))
+		{
+			System.out.println("these are equals");
+		}
+		else
+			System.out.println("these are not equals");
+			
+	
+	}
+
+}
