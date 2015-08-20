8e3be54d872e4574369c3068bdcfbb8f6ae99a58
ashutosh kumar <ashutoshimsec@gmail.com>
2015-08-20 14:28:53 +0530

    Create MyRightShift.java

---
+++ MyRightShift.java
@@ -0,0 +1,19 @@
+//package dataStructure;
+
+public class MyRightShift {
+
+
+
+	public static void main(String[] args)
+	{
+		 int x=4;
+		 int y= -4;
+		System.out.println(x>>1);
+		System.out.println(y>>1);
+	
+	}
+
+	
+}
+
+
